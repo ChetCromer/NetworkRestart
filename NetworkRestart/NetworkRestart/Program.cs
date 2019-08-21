99ea2199e25baa7abd053eed86d8e37ecefe81e6
Carr OConnor <carr.oconnor@outlook.com>
2019-08-21 11:07:04 -0400

    Switching to laptop for wifi

--- NetworkRestart/NetworkRestart/Program.cs
+++ NetworkRestart/NetworkRestart/Program.cs
@@ -1,10 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
+using System.Management;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NetworkRestart
 {
@@ -18,7 +15,26 @@ namespace NetworkRestart
             Console.WriteLine("Enter config file path");
             Console.WriteLine(configFilePath);
             LoadJson();
-            Console.ReadKey();
+
+            ConnectionOptions options = new ConnectionOptions();
+            options.Impersonation = System.Management.ImpersonationLevel.Impersonate;
+
+            ManagementScope scope = new ManagementScope("\\\\xxxx\\root\\cimv2", options);
+            scope.Connect();
+
+            ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+            ManagementObjectCollection queryCollection = searcher.Get();
+            foreach (ManagementObject m in queryCollection)
+            {
+                // Display the remote computer information
+                Console.WriteLine("Computer Name     : {0}", m["csname"]);
+                Console.WriteLine("Windows Directory : {0}", m["WindowsDirectory"]);
+                Console.WriteLine("Operating System  : {0}", m["Caption"]);
+                Console.WriteLine("Version           : {0}", m["Version"]);
+                Console.WriteLine("Manufacturer      : {0}", m["Manufacturer"]);
+            }
         }
 
         public static void LoadJson()
