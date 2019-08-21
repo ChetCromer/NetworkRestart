371a036c7ef6241f335a70dc9ea0c9c89c905633
Carr OConnor <carr.oconnor@outlook.com>
2019-08-21 11:14:45 -0400

    Added GetComputers

--- NetworkRestart/NetworkRestart/Program.cs
+++ NetworkRestart/NetworkRestart/Program.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Management;
 using System.IO;
+using System.DirectoryServices;
+using System.Collections.Generic;
 
 namespace NetworkRestart
 {
@@ -45,6 +47,31 @@ namespace NetworkRestart
                 var response = JsonConvert.DeserializeObject<RootObject>(json);
                 Console.WriteLine(response);
             }
+        }
+
+        public static List<string> GetComputers()
+        {
+            List<string> ComputerNames = new List<string>();
+
+            DirectoryEntry entry = new DirectoryEntry("OU=xxxx,OU=Computers,OU=Production,DC=xxxx,DC=local");
+            DirectorySearcher mySearcher = new DirectorySearcher(entry);
+            mySearcher.Filter = ("(objectClass=computer)");
+            mySearcher.SizeLimit = int.MaxValue;
+            mySearcher.PageSize = int.MaxValue;
+
+            foreach (SearchResult resEnt in mySearcher.FindAll())
+            {
+                //"CN=SGSVG007DC"
+                string ComputerName = resEnt.GetDirectoryEntry().Name;
+                if (ComputerName.StartsWith("CN="))
+                    ComputerName = ComputerName.Remove(0, "CN=".Length);
+                ComputerNames.Add(ComputerName);
+            }
+
+            mySearcher.Dispose();
+            entry.Dispose();
+
+            return ComputerNames;
         }
     }
 }
