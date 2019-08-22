dd3900c69c0005622df5570ba15f12829dcec8a0
Carr OConnor <carr.oconnor@gmail.com>
2019-08-21 20:22:01 -0400

    Ignore computers implemented

--- NetworkRestart/NetworkRestart/Program.cs
+++ NetworkRestart/NetworkRestart/Program.cs
@@ -9,69 +9,78 @@ namespace NetworkRestart
 {
     public class Program
     {
-        static string configFilePath = "C:\\NetworkRestart\\samples\\sampleconfigfile.json";
+        static string configFilePath = "C:\\Users\\carro\\Desktop\\ConfigFile-Wyndmoor.json";
         Program program = new Program();
         static void Main(string[] args)
         {
-            var configFilePath = "C:\\NetworkRestart\\samples\\sampleconfigfile.json";
             Console.WriteLine("Enter config file path");
             Console.WriteLine(configFilePath);
-            LoadJson();
-
-            ConnectionOptions options = new ConnectionOptions();
-            options.Impersonation = System.Management.ImpersonationLevel.Impersonate;
-
-            ManagementScope scope = new ManagementScope("\\\\xxxx\\root\\cimv2", options);
-            scope.Connect();
-
-            ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
-
-            ManagementObjectCollection queryCollection = searcher.Get();
-            foreach (ManagementObject m in queryCollection)
-            {
-                // Display the remote computer information
-                Console.WriteLine("Computer Name     : {0}", m["csname"]);
-                Console.WriteLine("Windows Directory : {0}", m["WindowsDirectory"]);
-                Console.WriteLine("Operating System  : {0}", m["Caption"]);
-                Console.WriteLine("Version           : {0}", m["Version"]);
-                Console.WriteLine("Manufacturer      : {0}", m["Manufacturer"]);
-            }
+            GetComputersToIgnore();
+            GetJsonData();
+            GetComputers();
         }
 
-        public static void LoadJson()
+        public static RootObject GetJsonData()
         {
             using (StreamReader r = new StreamReader(configFilePath))
             {
                 string json = r.ReadToEnd();
                 var response = JsonConvert.DeserializeObject<RootObject>(json);
-                Console.WriteLine(response);
+                return response;
             }
+        }
+
+        public static List<string> GetComputersToIgnore()
+        {
+            using (StreamReader r = new StreamReader(configFilePath))
+            {
+                string json = r.ReadToEnd();
+                var response = JsonConvert.DeserializeObject<RootObject>(json);
+                if (response == null) return new List<string>();
+                var computersToIgnore = new List<string>(response.computersToIgnore);
+
+                return computersToIgnore;
+            }
+
         }
 
         public static List<string> GetComputers()
         {
-            List<string> ComputerNames = new List<string>();
-
-            DirectoryEntry entry = new DirectoryEntry("OU=xxxx,OU=Computers,OU=Production,DC=xxxx,DC=local");
-            DirectorySearcher mySearcher = new DirectorySearcher(entry);
-            mySearcher.Filter = ("(objectClass=computer)");
-            mySearcher.SizeLimit = int.MaxValue;
-            mySearcher.PageSize = int.MaxValue;
-
-            foreach (SearchResult resEnt in mySearcher.FindAll())
+            using (StreamReader r = new StreamReader(configFilePath))
             {
-                //"CN=SGSVG007DC"
-                string ComputerName = resEnt.GetDirectoryEntry().Name;
-                if (ComputerName.StartsWith("CN="))
-                    ComputerName = ComputerName.Remove(0, "CN=".Length);
-                ComputerNames.Add(ComputerName);
-            }
+                string json = r.ReadToEnd();
+                var response = JsonConvert.DeserializeObject<RootObject>(json);
 
-            mySearcher.Dispose();
-            entry.Dispose();
+                List<string> ComputerNames = new List<string>();
 
-            return ComputerNames;
+                DirectoryEntry entry = new DirectoryEntry("LDAP://Wyndmoorals.local");
+                DirectorySearcher mySearcher = new DirectorySearcher(entry);
+                mySearcher.Filter = ("(objectClass=computer)");
+                mySearcher.SizeLimit = int.MaxValue;
+                mySearcher.PageSize = int.MaxValue;
+
+                foreach (SearchResult resEnt in mySearcher.FindAll())
+                {
+                    //"CN=SGSVG007DC"
+                    string ComputerName = resEnt.GetDirectoryEntry().Name;
+                    if (ComputerName.StartsWith("CN="))
+                        ComputerName = ComputerName.Remove(0, "CN=".Length);
+                    ComputerNames.Add(ComputerName);
+                }
+
+                mySearcher.Dispose();
+                entry.Dispose();
+
+                foreach (var i in ComputerNames)
+                {
+                    if (response.computersToIgnore.Contains(i))
+                    {
+                        ComputerNames.Remove(i);
+                    }
+                }
+
+                return ComputerNames;
+            }            
         }
     }
 }
